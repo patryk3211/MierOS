c0d3ca1ec1bf787900292df9fddef982534d1a9a
patryk3211 <patrykmierzy@gmail.com>
2022-03-07 10:47:40 +0100

    A couple of improvements

--- scripts/copy_sysroot.sh
+++ scripts/copy_sysroot.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-LOOP_DEV=$(udisksctl loop-setup --file hdd.img | grep -o '/[^ ]*' | head -c -2)
-MNT=$(udisksctl mount --block-device $(echo $LOOP_DEV)p1 | grep -o '/media[^ ]*' | head -c -2)
+mkdir imgmnt
 
-cp -r -u $1/* $MNT/
+cat $2 | sudo -S losetup -P /dev/loop100 hdd.img
+cat $2 | sudo -S mount /dev/loop100p1 imgmnt
 
-udisksctl unmount --block-device $(echo $LOOP_DEV)p1
-udisksctl loop-delete --block-device $LOOP_DEV
+#LOOP_DEV=$(udisksctl loop-setup --file hdd.img | grep -o '/[^ ]*' | head -c -2)
+#MNT=$(udisksctl mount --block-device $(echo $LOOP_DEV)p1 | grep -o '/media[^ ]*' | head -c -2)
+
+cp -r -u $1/* imgmnt/
+
+cat $2 | sudo -S umount imgmnt
+cat $2 | sudo -S losetup -d /dev/loop100
+
+#udisksctl unmount --block-device $(echo $LOOP_DEV)p1
+#udisksctl loop-delete --block-device $LOOP_DEV
+
+rmdir imgmnt
--- scripts/setupToolchain.sh
+++ scripts/setupToolchain.sh
@@ -13,7 +13,7 @@ GCC_NAME="gcc-10.2.0"
 GCC_PKG="$GCC_NAME.tar.gz"
 GCC_MD5="941a8674ea2eeb33f5c30ecf08124874"
 
-sudo apt-get install curl qemu binutils gcc g++ build-essential bison flex libgmp3-dev libmpc-dev libmpfr-dev texinfo hxtools autoconf automake-1.15 libtool
+sudo apt-get install cmake nasm curl qemu-system binutils gcc g++ build-essential bison flex libgmp3-dev libmpc-dev libmpfr-dev texinfo hxtools autoconf automake-1.15 libtool
 
 mkdir -p cross
 cd cross
