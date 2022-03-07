45ca105d878ddbc852c1a52bb5b5aac75db2a04c
patryk3211 <patrykmierzy@gmail.com>
2022-03-07 17:47:52 +0100

    Typed KBuffer

--- kernel/include/memory/kbuffer.hpp
+++ kernel/include/memory/kbuffer.hpp
@@ -2,6 +2,7 @@
 
 #include <types.h>
 #include <atomic.hpp>
+#include <utility.hpp>
 
 namespace kernel {
     class KBuffer {
@@ -20,5 +21,34 @@ namespace kernel {
 
         template<typename T = void> T* ptr() { return static_cast<T*>(raw_ptr); }
         template<typename T = void> const T* ptr() const { return static_cast<const T*>(raw_ptr); }
+    };
+
+    template<typename T> class TypedKBuffer {
+        KBuffer buffer;
+    public:
+        TypedKBuffer() : buffer(sizeof(T)) { }
+        TypedKBuffer(size_t element_count) : buffer(sizeof(T) * element_count) { }
+
+        TypedKBuffer(TypedKBuffer<T>&& other) : buffer(std::move(other.buffer)) { }
+        TypedKBuffer<T>& operator=(TypedKBuffer<T>&& other) {
+            buffer = std::move(other.buffer);
+        }
+
+        TypedKBuffer(const TypedKBuffer<T>& other) : buffer(other.buffer) { }
+        TypedKBuffer<T>& operator=(const TypedKBuffer<T>& other) {
+            buffer = other.buffer;
+        }
+
+        T* ptr() { return buffer.ptr<T>(); }
+        const T* ptr() const { return buffer.ptr<T>(); }
+    
+        T* operator->() { return buffer.ptr<T>(); }
+        const T* operator->() const { return buffer.ptr<T>(); }
+
+        T& operator*() { return *buffer.ptr<T>(); }
+        const T& operator*() const { return *buffer.ptr<T>(); }
+
+        T& operator[](size_t index) { return buffer.ptr<T>()[index]; }
+        const T& operator[](size_t index) const { return buffer.ptr<T>()[index]; }
     };
 }
