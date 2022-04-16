025d7ee246a137e07ae9d5079345a30dca607cdd
patryk3211 <patrykmierzy@gmail.com>
2022-04-16 09:55:25 +0200

    List fix

--- kernel/std/include/list.hpp
+++ kernel/std/include/list.hpp
@@ -196,8 +196,8 @@ namespace std {
             }
         }
 
-        T& front() { return head.next->value; }
-        T& back() { return tail.prev->value; }
+        T& front() { return static_cast<Entry*>(head.next)->value; }
+        T& back() { return static_cast<Entry*>(tail.prev)->value; }
 
         bool empty() { return length == 0; }
         unsigned int size() { return length; }
