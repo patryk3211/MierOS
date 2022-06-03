68ac3d03bf8d65f727ca609fcae93fb780b90ab1
patryk3211 <patrykmierzy@gmail.com>
2022-06-04 01:14:27 +0200

    Forgot to free memory after a test

--- kernel/src/tests/memory.cpp
+++ kernel/src/tests/memory.cpp
@@ -11,6 +11,7 @@ bool kernel::tests::pmm_test() {
         physaddr_t address2 = palloc(256);
         ASSERT_F(address == address2, "(PMM1) \033[1;37maddress2\033[0m should be the same as \033[1;37maddress\033[0m because it has been freed");
         if(address != address2) return false;
+        pfree(address2, 256);
     }
     return true;
 }
